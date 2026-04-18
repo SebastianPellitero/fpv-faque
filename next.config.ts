5ea2e1b8b3dfcf1fdd4594f7c0936a9064eae97d
SebastianPellitero <sebastian.pellitero@gmail.com>
2026-04-18 14:28:12 +0200

    fix not found error

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: NextConfig = {
-    basePath: '/fpv-faque',
+    assetPrefix: '/fpv-faque',
     images: {
         remotePatterns: [
             {
