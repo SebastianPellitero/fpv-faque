393f80a05e5e5f937cde33f891adf0ba64e6a6bc
SebastianPellitero <sebastian.pellitero@gmail.com>
2026-04-18 14:20:19 +0200

    integrating this page to sp domain

--- next.config.ts
+++ next.config.ts
@@ -1,21 +1,22 @@
-import type { NextConfig } from "next";
-import createNextIntlPlugin from "next-intl/plugin";
+import type { NextConfig } from 'next';
+import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
+const withNextIntl = createNextIntlPlugin('./src/i18n/request.ts');
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "i.ytimg.com",
-      },
-      {
-        protocol: "https",
-        hostname: "img.youtube.com",
-      },
-    ],
-  },
+    basePath: '/fpv-faque',
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'i.ytimg.com'
+            },
+            {
+                protocol: 'https',
+                hostname: 'img.youtube.com'
+            }
+        ]
+    }
 };
 
 export default withNextIntl(nextConfig);
